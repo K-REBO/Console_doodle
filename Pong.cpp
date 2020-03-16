ef72d17e4468cc0a5518fcc22d70704f4fd61149
K-REBO <58938944+K-REBO@users.noreply.github.com>
2020-03-16 19:11:52 +0900

    Update Pong.cpp

--- Pong.cpp
+++ Pong.cpp
@@ -1,13 +1,13 @@
 /*Pong solo version
-
 Attention!
-In this code 
+In this code
 Variable is wrote by Snake_Case
 Fugasious Variable in Function is wrote by UpperCamelCase
 */
 #include<iostream>
 #include<thread>
 #include<conio.h>
+#include<vector>
 #define X_field (16)
 #define Y_field (10)
 
@@ -21,6 +21,13 @@ Fugasious Variable in Function is wrote by UpperCamelCase
 #define Rg (6)
 #define UR (7)
 
+#define Space (0)
+#define UpWall (1)
+#define SideWall (2)
+
+
+using namespace std;
+
 int Y_Paddle = 8;
 
 //I gonna code to set Pong Randomly
@@ -29,19 +36,55 @@ int Y_Pong = 8;
 int Pong_dir = 2; //First Dirction
 // End Random 
 
-using namespace std;
+vector<vector<int>> Map
+{
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 
-void Message(string str){
+};
+
+vector<vector<char>> Pause_Menu
+{
+	{'+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','+','\n'},
+	{'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
+	{'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
+	{'|',' ',' ',' ',' ','R','e','s','t','e','r','t',' ',' ',' ',' ',' ','|','\n'},
+	{'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
+	{'|',' ',' ',' ',' ','R','e','s','u','m','e',' ',' ',' ',' ',' ',' ','|','\n'},
+	{'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
+	{'|',' ',' ',' ',' ','Q','u','i','t',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
+	{'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
+	{'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
+	{'+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','+','\n'}
+
+};
+
+void Message(string str)
+{
 	system("cls");
 	cout << str << endl;
-	getch();
+	_getch();
 }
 
 
 
 void Wall(int PongDir)
 {
-	switch(PongDir)
+	switch (PongDir)
 	{
 	case Up:break;
 	case UL:break;
@@ -54,10 +97,109 @@ void Wall(int PongDir)
 	}
 }
 
+void Display() {
+	for (int x = 0; x < X_field; ++x) 
+	{
+		for (int y = 0; y < Y_field; ++y)
+		{
+			cout << Map.at(x).at(y);
+		}
+		cout << "\n";
+	}
+	system("cls");
+	cout << flush;
+}
 
+void Pause() {
+	bool Game = false;
+	int Pos = 0;
 
+	while (!Game)
+	{
+		if (Pos == 0) 
+		{
+			for (int x = 0; x < 19; ++x) 
+			{
+				for (int y = 0; y < 11; ++y) 
+				{
+					if ((x == 13) && (y == 3))
+					{
+						cout << "<";
+					}
+					else {
+					cout << Pause_Menu.at(x).at(y);
+					}
+				}
+			}
+		cout << flush;
+		}
+
+		else if (Pos == 1) {
+			for (int x = 0; x < 19; ++x)
+			{
+				for (int y = 0; y < 11; ++y)
+				{
+					if ((x == 13) && (y == 5))
+					{
+						cout << "<";
+					}
+					else {
+						cout << Pause_Menu.at(x).at(y);
+					}
+				}
+			}
+		
+		}
+		else if (Pos == 2) {
+			for (int x = 0; x < 19; ++x)
+			{
+				for (int y = 0; y < 11; ++y)
+				{
+					if ((x == 13) && (y == 7))
+					{
+						cout << "<";
+					}
+					else {
+						cout << Pause_Menu.at(x).at(y);
+					}
+				}
+			}
+		
+		}
+
+		switch (_getch())
+		{
+			case 'l':if(Pos != 2)Pos++; break;
+			case 'r':if(Pos != 0)Pos--; break;
+			case ' ':Game = true; break;
+		}
+	}
+}
 int main(){
-	Message("Hello");
+	bool Game = false;
+	Message("Wellcome to Pong");
+	Message("Use L&R key to move Paddle \nSpace to show pause menu");
+	Message("Tap to start");
+	
+	while (!Game)
+	{
+		Display();
+
+		switch (_getch())
+		{
+		case 'l':
+			if (Y_Paddle < Y_field)
+				Y_Paddle++;
+			break;
+		case 'r':
+			if (Y_Paddle > 0)
+				Y_Paddle--;
+			break;
+		case ' ':
+			Pause();
+		}
+	}
+
 
 
 }
