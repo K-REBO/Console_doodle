ef0416ddb1bc4058e28c27ae0c781d806ba0d411
K-REBO <58938944+K-REBO@users.noreply.github.com>
2020-04-14 15:05:19 +0000

    New version

--- Pong.cpp
+++ Pong.cpp
@@ -1,206 +1,209 @@
-/*Pong solo version
-Attention!
-In this code
-Variable is wrote by Snake_Case
-Fugasious Variable in Function is wrote by UpperCamelCase
-*/
-#include<iostream>
-#include<thread>
-#include<conio.h>
+//+   1    +
+//          
+//          
+//    o     
+//          
+//0        2
+//          
+//    345     
+//    ===   
+//+        +
 #include<vector>
-#define X_field (16)
-#define Y_field (10)
-
-
-#define Up (0)
-#define UL (1)
-#define Le (2)
-#define DL (3)
-#define Dw (4)
-#define DR (5)
-#define Rg (6)
-#define UR (7)
-
-#define Space (0)
-#define UpWall (1)
-#define SideWall (2)
-
-
+#include<ncurses.h>
+#include<unistd.h>
 using namespace std;
 
-int Y_Paddle = 8;
 
-//I gonna code to set Pong Randomly
-int X_Pong = 6;// First Place
-int Y_Pong = 8;
-int Pong_dir = 2; //First Dirction
-// End Random 
+#define X_field (12)
+#define Y_field (12)
 
-vector<vector<int>> Map
+bool Game = false;//true (Game Over)  false (Game)
+
+int X_Pong = 5,Y_Pong = 5;
+int X_Bar = 5,Y_Bar = 9;
+int Pong_Cursor = 4;
+int Pong_state = 0;
+
+enum
 {
-	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
-	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-
+    Up,//UP            
+    UL,//UPper Left         0
+    Le,//Left            1  |  7
+    DL,//Downer Left      \ | /
+    Dw,//Down          2----+----6
+    DR,//Downer Right     / | \//
+    Ri,//Right           3  |  5
+    UR,//Upper Right        4
 };
 
-vector<vector<char>> Pause_Menu
+
+void Input(char c)
 {
-	{'+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','+','\n'},
-	{'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
-	{'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
-	{'|',' ',' ',' ',' ','R','e','s','t','e','r','t',' ',' ',' ',' ',' ','|','\n'},
-	{'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
-	{'|',' ',' ',' ',' ','R','e','s','u','m','e',' ',' ',' ',' ',' ',' ','|','\n'},
-	{'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
-	{'|',' ',' ',' ',' ','Q','u','i','t',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
-	{'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
-	{'|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|','\n'},
-	{'+','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','+','\n'}
+    switch (c)
+    {
+    case 'a':
+        if(X_Bar > 2)
+            X_Bar--;
+        break;
+    
+    case 'd':
+        if(X_Bar < X_field - 2)
+            X_Bar++;
+        break;
 
-};
+    default:
+        break;
+    }
+}
 
-void Message(string str)
+void Test(char x)
 {
-	system("cls");
-	cout << str << endl;
-	_getch();
+    switch(x)
+    {
+        case 'W':Y_Pong--;break;
+        case 'A':X_Pong--;break;
+        case 'D':X_Pong++;break;
+        case 'S':Y_Pong++;break;
+        default:          break;
+    }
 }
 
-
-
-void Wall(int PongDir)
+void PongCD()//Collid Detection Pong and Wall and Bar
 {
-	switch (PongDir)
-	{
-	case Up:break;
-	case UL:break;
-	case Le:break;
-	case DL:break;
-	case Dw:break;
-	case DR:break;
-	case Rg:break;
-	case UR:break;
-	}
+    //Left Wall
+    if(X_Pong == 1)
+        switch (Pong_Cursor)
+        {
+        case DL:Pong_Cursor = UR;break;
+        case Le:Pong_Cursor = Ri;break;
+        case UL:Pong_Cursor = DR;break;
+        default:                 break;
+        }
+
+    //Upper Wall
+    else if(Y_Pong == 1)
+        switch(Pong_Cursor)
+        {
+            case UR:Pong_Cursor = DR;break;
+            case Up:Pong_Cursor = Dw;break;
+            case UL:Pong_Cursor = DL;break;
+            default:                 break;
+        }
+    //Downer Wall
+    else if(Y_Pong == Y_field - 1)
+        Game = true;
+
+    //Right Wall
+    else if(X_Pong == X_field - 1)
+        switch(Pong_Cursor)
+        {
+            case DR:Pong_Cursor = UL;break;
+            case Ri:Pong_Cursor = Le;break;
+            case UR:Pong_Cursor = DL;break;
+            default:                 break;
+        }
+
+
+    //Left Bar
+    else if((X_Pong == X_Bar - 1)&&(Y_Pong == Y_Bar - 1))
+        switch (Pong_Cursor)
+        {
+        case DR:Pong_Cursor = UL;break;
+        case Dw:Pong_Cursor = UL;break;
+        case DL:Pong_Cursor = Up;break;
+        default:                 break;
+        }
+
+    //Middle Bar
+    else if((X_Pong == X_Bar)&&(Y_Pong == Y_Bar - 1))
+        switch (Pong_Cursor)
+        {
+        case DR:Pong_Cursor = UR;break;
+        case Dw:Pong_Cursor = Up;break;
+        case DL:Pong_Cursor = UL;break;
+        default:                 break;
+        }
+
+    //Right Bar
+    else if((X_Pong == X_Bar + 1)&&(Y_Pong == Y_Bar - 1))
+        switch(Pong_Cursor)
+        {
+            case DR:Pong_Cursor = Up;break;
+            case Dw:Pong_Cursor = UR;break;
+            case DL:Pong_Cursor = UR;break;
+            default:                 break;
+        }
+    
+    
 }
 
-void Display() {
-	for (int x = 0; x < X_field; ++x) 
-	{
-		for (int y = 0; y < Y_field; ++y)
-		{
-			cout << Map.at(x).at(y);
-		}
-		cout << "\n";
-	}
-	
-	cout << flush;
-}
-
-void Pause() {
-	bool Game = false;
-	int Pos = 0;
-
-	while (!Game)
-	{
-		if (Pos == 0) 
-		{
-			for (int x = 0; x < 20; ++x) 
-			{
-				for (int y = 0; y < 11; ++y) 
-				{
-					if ((x == 13) && (y == 3))
-					{
-						cout << "<";
-					}
-					else {
-					cout << Pause_Menu.at(x).at(y);
-					}
-				}
-			}
-		cout << flush;
-		}
-
-		else if (Pos == 1) {
-			for (int x = 0; x < 20; ++x)
-			{
-				for (int y = 0; y < 11; ++y)
-				{
-					if ((x == 13) && (y == 5))
-					{
-						cout << "<";
-					}
-					else {
-						cout << Pause_Menu.at(x).at(y);
-					}
-				}
-			}
-		
-		}
-		else if (Pos == 2) {
-			for (int x = 0; x < 20; ++x)
-			{
-				for (int y = 0; y < 11; ++y)
-				{
-					if ((x == 13) && (y == 7))
-					{
-						cout << "<";
-					}
-					else {
-						cout << Pause_Menu.at(x).at(y);
-					}
-				}
-			}
-		
-		}
-
-		switch (_getch())
-		{
-			case 'l':if(Pos != 2)Pos++; break;
-			case 'r':if(Pos != 0)Pos--; break;
-			case ' ':Game = true; break;
-		}
-	}
-}
-int main(){
-	bool Game = false;
-	Message("Wellcome to Pong");
-	Message("Use L&R key to move Paddle \nSpace to show pause menu");
-	Message("Tap to start");
-	
-	while (!Game)
-	{
-		system("cls");
-		Display();
-
-		switch (_getch())
-		{
-		case 'l':
-			if (Y_Paddle < Y_field)
-				Y_Paddle++;
-			break;
-		case 'r':
-			if (Y_Paddle > 0)
-				Y_Paddle--;
-			break;
-		case ' ':
-			Pause();
-		}
-	}
 
 
+
+int main()
+{
+    WINDOW *win;
+    char c;
+    initscr();
+    noecho();
+    cbreak();
+    timeout(100);
+    
+    //Display FIELD
+        mvaddch(0,0,'+');
+        mvaddch(0,X_field,'+');
+        mvaddch(Y_field,0,'+');
+        mvaddch(Y_field,X_field,'+');
+    //END
+
+    wrefresh(win);
+    while((c = getch()) != 0x1b)
+    {   
+        
+        mvaddch(Y_Pong,X_Pong,' '); //Clear Pong
+        mvaddch(Y_Bar,X_Bar - 1,' ');//Clear Bar
+        mvaddch(Y_Bar,X_Bar    ,' ');
+        mvaddch(Y_Bar,X_Bar + 1,' ');
+        
+    
+        Test(c);
+        PongCD();
+        Input(c);
+        
+        switch (Pong_Cursor)
+        {
+            case Up:         Y_Pong--;break;
+            case UL:X_Pong--;Y_Pong--;break;
+            case Le:X_Pong--;         break;
+            case DL:X_Pong--;Y_Pong++;break;
+            case Dw:         Y_Pong++;break;
+            case DR:X_Pong++;Y_Pong++;break;
+            case Ri:X_Pong++;         break;
+            case UR:X_Pong++;Y_Pong--;break;
+
+            default:
+                break;
+        }
+
+
+
+
+        mvaddch(Y_Pong,X_Pong  ,'o');//Display Pong
+        mvaddch(Y_Bar,X_Bar - 1,'=');//Display Bar
+        mvaddch(Y_Bar,X_Bar    ,'=');
+        mvaddch(Y_Bar,X_Bar + 1,'=');
+
+        
+        wrefresh(win);
+        if(Game)
+        {
+            erase();
+            mvaddstr(5,5,"Game Over");
+            break;
+        }
+    }
+    wclear(win);
+    wrefresh(win);
+    endwin();
 
 }
