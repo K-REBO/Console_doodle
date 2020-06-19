92be7892f778a875b8a70711e67bc617ce1603af
K-REBO <58938944+K-REBO@users.noreply.github.com>
2020-06-20 02:20:41 +0900

    Updating  a bit

--- main.rs
+++ main.rs
@@ -25,28 +25,27 @@ struct Pong {
     cursor: Cursor,
 }
 
-const X_field: i32 = 40; //四で割り切れる数
-const Y_field: i32 = 16;
-const WinPoint: u32 = 10; //First to get WinPoint is Winner
+const X_FIELD: i32 = 40; //四で割り切れる数
+const Y_FIELD: i32 = 16;
+const WIN_POINT: u32 = 10; //First to get WIN_POINT is winner
 
 fn main() {
     //Variables
-    let mut Winner: u8 = 0; //0 (In Game)   1 (1P Winner)   2(2P Winner)
-    let mut Turn: bool = false; //false (2P's Turn)   true(1P's Turn)
+    let mut turn: bool = false; //false (2P's turn)   true(1P's turn)
 
     let mut pong = Pong {
-        x: X_field / 2,
-        y: Y_field / 2,
+        x: X_FIELD / 2,
+        y: Y_FIELD / 2,
         cursor: Cursor::Le,
     };
-    let mut Bar1 = Bar {
+    let mut bar1 = Bar {
         x: 1,
-        y: Y_field / 2,
+        y: Y_FIELD / 2,
         score: 0,
     };
-    let mut Bar2 = Bar {
-        x: X_field - 1,
-        y: Y_field / 2,
+    let mut bar2 = Bar {
+        x: X_FIELD - 1,
+        y: Y_FIELD / 2,
         score: 0,
     };
     //END
@@ -57,17 +56,17 @@ fn main() {
 
     //Display Field
     mvprintw(0, 0, "+");
-    mvprintw(0, X_field, "+");
-    mvprintw(Y_field, 0, "+");
-    mvprintw(Y_field, X_field, "+");
+    mvprintw(0, X_FIELD, "+");
+    mvprintw(Y_FIELD, 0, "+");
+    mvprintw(Y_FIELD, X_FIELD, "+");
     //END
 
     //First Message
-    mvprintw(Y_field / 2, X_field / 2 - 7, "Wellcome 2 Pong");
+    mvprintw(Y_FIELD / 2, X_FIELD / 2 - 7, "Wellcome 2 Pong");
     refresh();
     getch();
-    mvprintw(Y_field / 2, X_field / 2 - 7, "1 Player use A & D");
-    mvprintw(Y_field / 2 + 1, X_field / 2 - 7, "2 Player use J & L");
+    mvprintw(Y_FIELD / 2, X_FIELD / 2 - 7, "1 Player use A & D");
+    mvprintw(Y_FIELD / 2 + 1, X_FIELD / 2 - 7, "2 Player use J & L");
     refresh();
     getch();
     erase();
@@ -77,264 +76,284 @@ fn main() {
 
     //Display Field
     mvprintw(0, 0, "+");
-    mvprintw(0, X_field, "+");
-    mvprintw(Y_field, 0, "+");
-    mvprintw(Y_field, X_field, "+");
+    mvprintw(0, X_FIELD, "+");
+    mvprintw(Y_FIELD, 0, "+");
+    mvprintw(Y_FIELD, X_FIELD, "+");
     refresh();
     //END
 
-    let mut stri: &str;
-
     loop {
-
-        let mut ch = getch();
+        let ch = getch();
 
         mvprintw(pong.y, pong.x, " "); //Clear Pong(Ovrewrite with whitespace)
 
-        mvprintw(Bar1.y - 1, Bar1.x, " "); //Clear 1P's Bar
-        mvprintw(Bar1.y, Bar1.x, " ");
-        mvprintw(Bar1.y + 1, Bar1.x, " ");
+        mvprintw(bar1.y - 1, bar1.x, " "); //Clear 1P's Bar
+        mvprintw(bar1.y, bar1.x, " ");
+        mvprintw(bar1.y + 1, bar1.x, " ");
 
-        mvprintw(Bar2.y - 1, Bar2.x, " "); //Clear 2P's Bar
-        mvprintw(Bar2.y, Bar2.x, " ");
-        mvprintw(Bar2.y + 1, Bar2.x, " ");
+        mvprintw(bar2.y - 1, bar2.x, " "); //Clear 2P's Bar
+        mvprintw(bar2.y, bar2.x, " ");
+        mvprintw(bar2.y + 1, bar2.x, " ");
 
-        mvprintw(1, X_field / 2, if Turn { "1P" } else { "2P" }); //Display Turn Player
+        mvprintw(1, X_FIELD / 2, if turn { "1P" } else { "2P" }); //Display turn Player
 
         //Display Score
-        mvprintw(2, X_field / 4, &Bar2.score.to_string());
-        mvprintw(2, (X_field / 4) * 3, &Bar1.score.to_string());
+        mvprintw(2, X_FIELD / 4, &bar2.score.to_string());
+        mvprintw(2, (X_FIELD / 4) * 3, &bar1.score.to_string());
 
-        
-        pongCD(&mut pong,&mut Bar1,&mut Bar2,Turn);
-        // 
-        input(ch,&mut Bar1,&mut Bar2,Turn);
-
-
-
+        pong_cd(&mut pong, &mut bar1, &mut bar2, turn);
+        //
+        input(ch, &mut bar1, &mut bar2, turn);
 
         match &mut pong.cursor {
-            Cursor::Up => {            pong.y -= 1;             },
-            Cursor::UL => {pong.x -= 1;pong.y -= 1;Turn = true; },
-            Cursor::Le => {pong.x -= 1;            Turn = true; },
-            Cursor::DL => {pong.x -= 1;pong.y += 1;Turn = true; },
-            Cursor::Dw => {            pong.y += 1;             },
-            Cursor::DR => {pong.x += 1;pong.y += 1;Turn = false;},
-            Cursor::Ri => {pong.x += 1;            Turn = false;},
-            Cursor::UR => {pong.x += 1;pong.y -= 1;Turn = false;},
+            Cursor::Up => {
+                pong.y -= 1;
+            }
+            Cursor::UL => {
+                pong.x -= 1;
+                pong.y -= 1;
+                turn = true;
+            }
+            Cursor::Le => {
+                pong.x -= 1;
+                turn = true;
+            }
+            Cursor::DL => {
+                pong.x -= 1;
+                pong.y += 1;
+                turn = true;
+            }
+            Cursor::Dw => {
+                pong.y += 1;
+            }
+            Cursor::DR => {
+                pong.x += 1;
+                pong.y += 1;
+                turn = false;
+            }
+            Cursor::Ri => {
+                pong.x += 1;
+                turn = false;
+            }
+            Cursor::UR => {
+                pong.x += 1;
+                pong.y -= 1;
+                turn = false;
+            }
         }
 
-        mvprintw(pong.y,pong.x,"o");//Display PONG
+        mvprintw(pong.y, pong.x, "o"); //Display PONG
 
-        mvprintw(Bar1.y - 1,Bar1.x,"|");//Display 1P Bar
-        mvprintw(Bar1.y    ,Bar1.x,"|");
-        mvprintw(Bar1.y + 1,Bar1.x,"|");
+        mvprintw(bar1.y - 1, bar1.x, "|"); //Display 1P Bar
+        mvprintw(bar1.y, bar1.x, "|");
+        mvprintw(bar1.y + 1, bar1.x, "|");
 
-        mvprintw(Bar2.y - 1,Bar2.x,"|");//Display 2P Bar
-        mvprintw(Bar2.y    ,Bar2.x,"|");
-        mvprintw(Bar2.y + 1,Bar2.x,"|");
+        mvprintw(bar2.y - 1, bar2.x, "|"); //Display 2P Bar
+        mvprintw(bar2.y, bar2.x, "|");
+        mvprintw(bar2.y + 1, bar2.x, "|");
 
         refresh();
-
 
         //27 mean escape
         if ch == 27 {
             break;
         }
-        if Bar2.score == WinPoint || Bar1.score == WinPoint {
+        if bar2.score == WIN_POINT || bar1.score == WIN_POINT {
             erase();
             timeout(-1);
 
-            if Bar1.score == WinPoint {
-                Winner = 1;
-            }
-            else {
-                Winner = 2;
-            }
 
-            mvprintw(Y_field / 2,3,{
-                if Winner == 1 {
-                    "1P WIN \n Congrats"
-                }
-                else {
-                    "2P WIN \n Congrats"
+            mvprintw(Y_FIELD / 2 - 1,X_FIELD / 2 - 3, {
+                if bar1.score == WIN_POINT {
+                    "1P WIN"
+                } else {
+                    "2P WIN"
                 }
             });
+            mvprintw(Y_FIELD / 2 + 1,X_FIELD / 2 - 4,"Congrats");
 
             getch();
-            break();
+            break ();
         }
-
-
-
     }
 
-    
     refresh();
     getch();
     endwin();
 }
-fn input(ch: i32,Bar1: &mut Bar ,Bar2: &mut Bar,Turn: bool) {
-    let ch = convert(ch);// i32 --> char 
+fn input(ch: i32, bar1: &mut Bar, bar2: &mut Bar, turn: bool) {
+    let ch = convert(ch); // i32 --> char
 
-    if Turn {
+    if turn {
         match ch {
             'a' => {
-                if Bar1.y + 1 < Y_field - 1 {
-                    Bar1.y += 1;
+                if bar1.y + 1 < Y_FIELD - 1 {
+                    bar1.y += 1;
                 }
-            },
+            }
             'd' => {
-                if Bar1.y -1 > 1 {
-                    Bar1.y -= 1;
+                if bar1.y - 1 > 1 {
+                    bar1.y -= 1;
                 }
-            },
+            }
             _ => (),
         }
-    }
-
-    else {
+    } else {
         match ch {
             'j' => {
-                if Bar2.y + 1 < Y_field -1 {
-                    Bar2.y += 1;
+                if bar2.y + 1 < Y_FIELD - 1 {
+                    bar2.y += 1;
                 }
-            },
+            }
             'l' => {
-                if Bar2.y -1 > 1 {
-                    Bar2.y -= 1;
+                if bar2.y - 1 > 1 {
+                    bar2.y -= 1;
                 }
-            },
+            }
             _ => (),
         }
     }
-
-
 }
-fn pongCD(pong:&mut Pong,Bar1:&mut Bar,Bar2:&mut Bar,Turn: bool) {
-    
+fn pong_cd(pong: &mut Pong, bar1: &mut Bar, bar2: &mut Bar, turn: bool) {
     //Left Wall
     if pong.x <= 1 {
-        Bar1.score += 1;
-        pong.x = X_field / 2;
-        pong.y = Y_field / 2;
+        bar1.score += 1;
+        pong.x = X_FIELD / 2;
+        pong.y = Y_FIELD / 2;
         pong.cursor = {
-            if Turn {
+            if turn {
                 Cursor::Le
-            }
-            else {
+            } else {
                 Cursor::Ri
             }
         };
     }
 
     //Upper Wall
-     if pong.y <= 1 {
+    if pong.y <= 1 {
         match pong.cursor {
-            Cursor::UR => {pong.cursor = Cursor::DR;},
-            Cursor::Up => {pong.cursor = Cursor::Dw;},
-            Cursor::UL => {pong.cursor = Cursor::DL;},
-            _          => (),
+            Cursor::UR => {
+                pong.cursor = Cursor::DR;
+            }
+            Cursor::Up => {
+                pong.cursor = Cursor::Dw;
+            }
+            Cursor::UL => {
+                pong.cursor = Cursor::DL;
+            }
+            _ => (),
         }
     }
 
     //Right Wall
-     if pong.x >= X_field - 1 {
-        Bar2.score += 1;
-        pong.x = X_field / 2;
-        pong.y = Y_field / 2;
+    if pong.x >= X_FIELD - 1 {
+        bar2.score += 1;
+        pong.x = X_FIELD / 2;
+        pong.y = Y_FIELD / 2;
         pong.cursor = {
-            if Turn {
+            if turn {
                 Cursor::Le
-            }
-            else {
+            } else {
                 Cursor::Ri
             }
         };
     }
 
     //Downer Wall
-     if pong.y >= Y_field -1 {
+    if pong.y >= Y_FIELD - 1 {
         match pong.cursor {
-            Cursor::DR => {pong.cursor = Cursor::UR;},
-            Cursor::Dw => {pong.cursor = Cursor::Up;},
-            Cursor::DL => {pong.cursor = Cursor::UL;},
-            _          => (),
+            Cursor::DR => {
+                pong.cursor = Cursor::UR;
+            }
+            Cursor::Dw => {
+                pong.cursor = Cursor::Up;
+            }
+            Cursor::DL => {
+                pong.cursor = Cursor::UL;
+            }
+            _ => (),
         }
     }
 
     // 1Bar Upper
-     if (pong.y == Bar1.y + 1)&&(pong.x == Bar1.x + 1) {
+    if (pong.y == bar1.y + 1) && (pong.x == bar1.x + 1) {
         match pong.cursor {
-            Cursor::DL => {pong.cursor = Cursor::Ri;},
-            Cursor::Le => {pong.cursor = Cursor::DR;},
-            Cursor::UL => {pong.cursor = Cursor::DR;},
-            _          => (),
+            Cursor::DL => {
+                pong.cursor = Cursor::Ri;
+            }
+            Cursor::Le => {
+                pong.cursor = Cursor::DR;
+            }
+            Cursor::UL => {
+                pong.cursor = Cursor::DR;
+            }
+            _ => (),
         }
     }
 
     //1Bar Middle
-     if (pong.y == Bar1.y)&&(pong.x == Bar1.x + 1){
+    if (pong.y == bar1.y) && (pong.x == bar1.x + 1) {
         match pong.cursor {
             Cursor::DL => pong.cursor = Cursor::DR,
             Cursor::Le => pong.cursor = Cursor::Ri,
             Cursor::UL => pong.cursor = Cursor::UR,
-            _          => (),
+            _ => (),
         }
     }
-    
+
     //1Bar Downner
-     if (pong.y == Bar1.y - 1)&&(pong.x == Bar1.x + 1) {
+    if (pong.y == bar1.y - 1) && (pong.x == bar1.x + 1) {
         match pong.cursor {
             Cursor::DL => pong.cursor = Cursor::UR,
             Cursor::Le => pong.cursor = Cursor::UR,
             Cursor::UL => pong.cursor = Cursor::Ri,
-            _          => (),
+            _ => (),
         }
     }
 
-    //2Bar Upper 
-     if (pong.y == Bar2.y + 1)&&(pong.x == Bar2.x -1){
+    //2Bar Upper
+    if (pong.y == bar2.y + 1) && (pong.x == bar2.x - 1) {
         match pong.cursor {
             Cursor::DR => pong.cursor = Cursor::Le,
-            Cursor::Ri => {pong.cursor = Cursor::DL},
+            Cursor::Ri => pong.cursor = Cursor::DL,
             Cursor::UR => pong.cursor = Cursor::DL,
-            _          => (),
+            _ => (),
         }
     }
 
     //2Bar Middle
-     if (pong.y == Bar2.y)&&(pong.x == Bar2.x - 1) {
+    if (pong.y == bar2.y) && (pong.x == bar2.x - 1) {
         match pong.cursor {
             Cursor::DR => pong.cursor = Cursor::DL,
             Cursor::Ri => pong.cursor = Cursor::Le,
             Cursor::UR => pong.cursor = Cursor::UL,
-            _          => (),
+            _ => (),
         }
     }
 
     //2Bar Downner
-     if (pong.y == Bar2.y - 1)&&(pong.x == Bar2.x - 1)
-    {
+    if (pong.y == bar2.y - 1) && (pong.x == bar2.x - 1) {
         match pong.cursor {
-            Cursor::DR => {pong.cursor = Cursor::UL;},
-            Cursor::Ri => {pong.cursor = Cursor::UL;},
-            Cursor::UR => {pong.cursor = Cursor::Le;},
-            _          => (),
+            Cursor::DR => {
+                pong.cursor = Cursor::UL;
+            }
+            Cursor::Ri => {
+                pong.cursor = Cursor::UL;
+            }
+            Cursor::UR => {
+                pong.cursor = Cursor::Le;
+            }
+            _ => (),
         }
     }
-
-
-
-
 }
 
-fn convert(ch:i32) -> char {
+fn convert(ch: i32) -> char {
     match ch {
-        97  => 'a',
+        97 => 'a',
         100 => 'd',
         106 => 'j',
         108 => 'l',
-        _   => ' ',
+        _ => ' ',
     }
 }
